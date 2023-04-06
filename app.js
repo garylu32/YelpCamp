026fa9a22ba4767e9826a10d6f4c7d30194229b8
Chenhao Lu <garylu32@gmail.com>
2023-04-06 01:24:04 -0700

    bug fixed

--- app.js
+++ app.js
@@ -2,11 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-});
+mongoose.connect("mongodb://localhost:27017/yelp-camp");
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
